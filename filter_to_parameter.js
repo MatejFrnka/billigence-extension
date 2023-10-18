7a427d6b45cdf4e54fad6b346badc1a462c9b1eb
Matěj Frnka <mat.frnka@gmail.com>
2023-10-18 16:39:59 +0200

    added comments

--- filter_to_parameter.js
+++ filter_to_parameter.js
@@ -6,13 +6,24 @@
   const PARAMETER_ALL_VALUE = "All"
   const CONFIGURE_PATH = `/billigence-extension/config.html`;
 
-  let resettingFilters = false;
+  /**
+   * stops the filter changed handler from being called multiple times
+   * @type {boolean}
+   */
+  let resettingFilters = false; // todo maybe removing this could change something
 
+  /**
+   * Utility function that returns the saved configuration of extension
+   * @returns {any}
+   */
   function getFilterParamPairs() {
     const pairsString = tableau.extensions.settings.get("pairs")
     return JSON.parse(pairsString);
   }
 
+  /**
+   * Initialization of extension
+   */
   $(document).ready(function () {
     tableau.extensions.initializeAsync({'configure': configure}).then(function () {
       initializeListeners();
@@ -35,23 +46,31 @@
     });
   }
 
+  /**
+   * Adds filter changed listeners to every worksheet in dashboard the extensions is running in
+   */
   function initializeListeners() {
     // To get filter info, first get the dashboard.
-    const dashboard = tableau.extensions.dashboardContent.dashboard;
+    const dashboard = tableau.extensions.dashboardContent.dashboard; // todo possible to change this from dashboard to something else
 
     dashboard.worksheets.forEach(function (worksheet) {
       worksheet.addEventListener(tableau.TableauEventType.FilterChanged, filterChangedHandler);
     });
   }
 
-  // This is a handling function that is called anytime a filter is changed in Tableau.
+  /**
+   * This is a handling function that is called anytime a filter is changed in Tableau.
+   * @param filterEvent event provided by tableau extension api
+   * @returns {Promise<void>}
+   */
   async function filterChangedHandler(filterEvent) {
+    // return if filter resetting is already in progress
     if (resettingFilters) {
       return
     }
     try {
       // find parameter this filter is supposed to change
-      let pairIndex = findFilterPairIndex(filterEvent.fieldName)
+      let pairIndex = findFilterPairIndex(filterEvent.fieldName) // todo: maybe this finds a filter that it is not supposed to find and resets - probably not
       // if any parameter is found
       if (pairIndex !== null) {
         // get object Filter, pass it to updateParameter
@@ -63,6 +82,11 @@
     }
   }
 
+  /**
+   * find filter pair with matching filterName to the parameter
+   * @param filterName
+   * @returns {number|null} index of pair to change
+   */
   function findFilterPairIndex(filterName) {
     const filterParamPairs = getFilterParamPairs();
     for (let i = 0; i < filterParamPairs.length; i++) {
@@ -73,14 +97,17 @@
     return null;
   }
 
-  // Accepts
-  // * object Filter: https://tableau.github.io/extensions-api/docs/interfaces/filter.html
-  // * string with name of the parameter to be updated
-  // Updates parameter with given name to value in filter.
+
+  /**
+   * Updates parameter with given name to value in filter.
+   * @param filter: object Filter: https://tableau.github.io/extensions-api/docs/interfaces/filter.html
+   * @param pairIndex int index of the parameter to be updated
+   * @returns {Promise<void>}
+   */
   async function updateParameter(filter, pairIndex) {
 
     // To get filter info, first get the dashboard.
-    const dashboard = tableau.extensions.dashboardContent.dashboard;
+    const dashboard = tableau.extensions.dashboardContent.dashboard; // todo - is it right to deal with dashboards here?
     const parameterName = getFilterParamPairs()[pairIndex].param
     let param = await dashboard.findParameterAsync(parameterName)
     // skip if parameter is null
@@ -99,13 +126,18 @@
 
   // Accepts
   // * int that states index to start resetting filters from in filterParamPairs array
+  /**
+   * Resets all filters from `getFilterParamPairs();` starting at `resetFrom` until the end
+   * @param resetFrom index to start resetting filters from in filterParamPairs array
+   * @returns {Promise<void>}
+   */
   async function resetFilters(resetFrom) {
     resettingFilters = true;
     const filterParamPairs = getFilterParamPairs();
     try {
       const parameterPromises = [];
       // To get filter info, first get the dashboard.
-      const dashboard = tableau.extensions.dashboardContent.dashboard;
+      const dashboard = tableau.extensions.dashboardContent.dashboard; // todo maybe not dashboard
 
       for (let i = resetFrom; i < filterParamPairs.length; i++) {
         parameterPromises.push(dashboard.findParameterAsync(filterParamPairs[i].param))
@@ -115,11 +147,11 @@
       for (let i = 0; i < returnedParams.length; i++) {
         // Reset parameter
         returnedParams[i].changeValueAsync(PARAMETER_ALL_VALUE)
-          .catch(_ => alert(`Parameter ${returnedParams[i].name}, (id: ${returnedParams[i].id}) cannot be reset to value ${PARAMETER_ALL_VALUE}`))
+          .catch(_ => console.error(`Parameter ${returnedParams[i].name}, (id: ${returnedParams[i].id}) cannot be reset to value ${PARAMETER_ALL_VALUE}`))
 
         // Reset filters in every worksheet
         let filterName = filterParamPairs[i + resetFrom].filter
-
+        // todo this might be causing the issue
         for (let j = 0; j < dashboard.worksheets.length; j++) {
           dashboard.worksheets[j].clearFilterAsync(filterName).catch(_ => {
             // don't do anything - errors will be thrown for every worksheet that doesn't have the filter
