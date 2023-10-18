a95c0e740982c9a228dbf3a582218ccf01cba77a
Matěj Frnka <mat.frnka@gmail.com>
2023-10-18 17:37:46 +0200

    added a lot of debugging console logs

--- filter_to_parameter.js
+++ filter_to_parameter.js
@@ -18,6 +18,10 @@
    */
   function getFilterParamPairs() {
     const pairsString = tableau.extensions.settings.get("pairs")
+    if (pairsString === undefined || pairsString === null) {
+      console.error("Loading FilterParamPairs but plugin hasn't been configured yet")
+      return []
+    }
     return JSON.parse(pairsString);
   }
 
@@ -25,8 +29,12 @@
    * Initialization of extension
    */
   $(document).ready(function () {
+    console.log("tableau extension initialization")
     tableau.extensions.initializeAsync({'configure': configure}).then(function () {
+      console.log("tableau extension initialized")
+      console.log("initializing listeners")
       initializeListeners();
+      console.log("initialized listeners")
     }, function (err) {
       // Something went wrong in initialization.
       console.error('Error while Initializing: ' + err.toString());
@@ -52,7 +60,6 @@
   function initializeListeners() {
     // To get filter info, first get the dashboard.
     const dashboard = tableau.extensions.dashboardContent.dashboard; // todo possible to change this from dashboard to something else
-
     dashboard.worksheets.forEach(function (worksheet) {
       worksheet.addEventListener(tableau.TableauEventType.FilterChanged, filterChangedHandler);
     });
@@ -66,11 +73,14 @@
   async function filterChangedHandler(filterEvent) {
     // return if filter resetting is already in progress
     if (resettingFilters) {
+      console.log("Event ignore because filters are being reset - " + filterEvent.fieldName)
       return
     }
     try {
       // find parameter this filter is supposed to change
+      console.log("finding pair for " + filterEvent.fieldName)
       let pairIndex = findFilterPairIndex(filterEvent.fieldName) // todo: maybe this finds a filter that it is not supposed to find and resets - probably not
+      console.log(pairIndex)
       // if any parameter is found
       if (pairIndex !== null) {
         // get object Filter, pass it to updateParameter
@@ -105,17 +115,20 @@
    * @returns {Promise<void>}
    */
   async function updateParameter(filter, pairIndex) {
-
+    console.log("param " + getFilterParamPairs()[pairIndex].param + ", filter " + "param " + getFilterParamPairs()[pairIndex].filter + " is being updated")
     // To get filter info, first get the dashboard.
     const dashboard = tableau.extensions.dashboardContent.dashboard; // todo - is it right to deal with dashboards here?
     const parameterName = getFilterParamPairs()[pairIndex].param
     let param = await dashboard.findParameterAsync(parameterName)
+    console.log(param)
     // skip if parameter is null
     if (!(param === null || param === undefined)) {
       if (filter.isAllSelected !== true && filter.appliedValues.length !== 0) {
+        console.log("changing param value to " + filter.appliedValues[0].value)
         param.changeValueAsync(filter.appliedValues[0].value)
       } else if (filter.isAllSelected === true) {
         // if filter is set to select everything, set parameter to all
+        console.log("changing param value to ALL")
         param.changeValueAsync(PARAMETER_ALL_VALUE)
       } else {
         console.error("Unkown value being set")
@@ -127,11 +140,12 @@
   // Accepts
   // * int that states index to start resetting filters from in filterParamPairs array
   /**
-   * Resets all filters from `getFilterParamPairs();` starting at `resetFrom` until the end
+   * Resets all filters and parameters from `getFilterParamPairs();` starting at `resetFrom` until the end
    * @param resetFrom index to start resetting filters from in filterParamPairs array
    * @returns {Promise<void>}
    */
   async function resetFilters(resetFrom) {
+    console.log("resetting filters and parameters from index " + resetFrom)
     resettingFilters = true;
     const filterParamPairs = getFilterParamPairs();
     try {
@@ -144,26 +158,40 @@
       }
 
       let returnedParams = await Promise.all(parameterPromises);
+      let filterClearPromises = []
       for (let i = 0; i < returnedParams.length; i++) {
+        console.log("reseting parameter " + returnedParams[i].name + " to ALL value:");
+        console.log(returnedParams[i]);
         // Reset parameter
         returnedParams[i].changeValueAsync(PARAMETER_ALL_VALUE)
           .catch(_ => console.error(`Parameter ${returnedParams[i].name}, (id: ${returnedParams[i].id}) cannot be reset to value ${PARAMETER_ALL_VALUE}`))
 
         // Reset filters in every worksheet
         let filterName = filterParamPairs[i + resetFrom].filter
-        // todo this might be causing the issue
+        // todo this might be causing the issue - it would be better to know which worksheet the filter is in and only search that one.
         for (let j = 0; j < dashboard.worksheets.length; j++) {
-          dashboard.worksheets[j].clearFilterAsync(filterName).catch(_ => {
-            // don't do anything - errors will be thrown for every worksheet that doesn't have the filter
-            // this may reset filters on other worksheets than the intended ones. Should this be applied to only one worksheet or to all?
-            // it would be better to do it for one specific worksheet to improve performance for bigger projects
-          });
+          console.log("clearing filter " + filterName)
+          let promise = dashboard.worksheets[j].clearFilterAsync(filterName)
+            .then((e) => {
+              console.log(`filter ${e} cleared`)
+            })
+            .catch(_ => {
+              console.log("filter " + filterName + " from worksheet " + dashboard.worksheets[j].name + " not found (it shouldn't be an issue)")
+              // don't do anything - errors will be thrown for every worksheet that doesn't have the filter
+              // this may reset filters on other worksheets than the intended ones. Should this be applied to only one worksheet or to all?
+              // it would be better to do it for one specific worksheet to improve performance for bigger projects
+            });
+
+          filterClearPromises.push(promise)
         }
       }
+      await Promise.all(filterClearPromises);
     } catch (e) {
       console.error(e)
     } finally {
+      console.log("filters no longer being reset")
       resettingFilters = false;
     }
+    console.log("filter reset end")
   }
 })();
